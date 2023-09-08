c0686504b7c7aa04e58421983dc1534838f73f22
SAJAL KHAWAS <khawassajal756@gmail.com>
2023-09-08 18:31:36 +0530

    e-commerece page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // screens:{
+      //   '2xl': '1436px'
+      // },
+      colors:{
+        'primaryBlue': '#6497ce'
+      },
+      fontFamily:{
+        'jost': ['Jost', "sans-serif", ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
